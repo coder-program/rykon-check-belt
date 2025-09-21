7f60c5bdc6c34b1c9114568b6b52a5a534a723f5
Deleon <deleon.leite@agricultura.gov.br>
2025-09-21 12:37:09 -0300

    fix: Ajusta configuração do TypeORM para suportar conexão via socket Unix do Cloud SQL

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -6,8 +6,15 @@ config();
 
 export default new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432', 10),
+  ...(process.env.DB_SOCKET
+    ? {
+        host: process.env.DB_SOCKET,
+        extra: { socketPath: process.env.DB_SOCKET }
+      }
+    : {
+        host: process.env.DB_HOST || 'localhost',
+        port: parseInt(process.env.DB_PORT || '5432', 10)
+      }),
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS || 'postgres',
   database: process.env.DB_NAME || 'acesso_usuarios_db',
