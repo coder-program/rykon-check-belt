6b56bb523668c4104d0b3cdb668b166cc04093b0
Deleon <deleon.leite@agricultura.gov.br>
2025-09-24 10:01:13 -0300

    feat: adiciona configuração do TypeORM para produção

---
+++ backend/ormconfig.ts
@@ -0,0 +1,19 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
+
+export default new DataSource({
+  type: 'postgres',
+  host: DB_HOST || '/cloudsql/teamcruz-controle-alunos:southamerica-east1:teamcruz-db',
+  username: DB_USER || 'teamcruz_app',
+  password: DB_PASS || 'TeamCruz2024@',
+  database: DB_NAME || 'teamcruz_db',
+  entities: ['dist/src/**/*.entity.js'],
+  migrations: ['dist/src/migrations/*.js'],
+  migrationsTableName: 'migrations',
+  synchronize: false,
+  ssl: process.env.NODE_ENV === 'production',
+});
