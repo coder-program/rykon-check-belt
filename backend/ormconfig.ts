9fb27a844f4fe0a2df9d997b32ef457da1dbc9bb
Deleon <deleon.leite@agricultura.gov.br>
2025-09-28 16:43:01 -0300

    fix: update database host configuration

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -7,7 +7,7 @@ const { DB_HOST, DB_USER, DB_PASS, DB_NAME } = process.env;
 
 export default new DataSource({
   type: 'postgres',
-  host: DB_HOST || '34.95.254.51',
+  host: DB_HOST || '34.39.173.213',
   port: parseInt(process.env.DB_PORT || '5432'),
   username: DB_USER || 'teamcruz_app',
   password: DB_PASS || 'TeamCruz2024@',
